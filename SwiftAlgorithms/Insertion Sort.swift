cba82e177b975f228d15ce3486337e15e2158e7e
Matthew Lin <Lin.M@etoncollege.org.uk>
2023-11-03 11:20:44 +0000

    Worked on Quick Sort

--- SwiftAlgorithms/Insertion Sort.swift
+++ SwiftAlgorithms/Insertion Sort.swift
@@ -1,9 +1,9 @@
-func InsertionSort(array: inout [Int]) {
-    print(array)
-    for i in 
-    if array[1] < array[0] {
-        array.insert(array[1], at: 0)
-        array.remove(at: 2)
-    }
-    print(array)
-}
+//func InsertionSort(array: inout [Int]) {
+//    print(array)
+//    for i in
+//    if array[1] < array[0] {
+//        array.insert(array[1], at: 0)
+//        array.remove(at: 2)
+//    }
+//    print(array)
+//}
--- SwiftAlgorithms/main.swift
+++ SwiftAlgorithms/main.swift
@@ -9,4 +9,25 @@ import Foundation
 
 var array1 = [10, 9, 3, 5, 6, 1, 2]
 
-InsertionSort(array: &array1)
+func QuickSort(array: inout [Int]) {
+    let pivot = array[0]
+    var leftArray = [Int]()
+    var rightArray = [Int]()
+    for i in 1...(array.count - 1) {
+        if array[i] < pivot {
+            leftArray.append(array[i])
+        } else {
+            rightArray.append(array[i])
+        }
+    }
+    print(leftArray)
+    print(rightArray)
+    if leftArray.count <= 1 {
+        return leftArray
+    } else {
+        QuickSort(array: &leftArray)
+    }
+}
+
+
+QuickSort(array: &array1)
